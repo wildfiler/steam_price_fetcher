4be7e1cafb1b6a63a80dd624cd860774e105ce22
Alexey Neischenko <alexneisc@gmail.com>
2016-10-08 15:52:16 +0300

    Add indentation to SteamRequestLimiter

--- src/services/steam_request_limiter.cr
+++ src/services/steam_request_limiter.cr
@@ -3,30 +3,30 @@ require "redis"
 
 class SteamRequestLimiter
   def limit : HTTP::Client::Response
-  try = 0
-  while try < 20
-    if get_request_count > 18
-      sleep(61 - Time.now.second)
-    end
+    try = 0
+    while try < 20
+      if get_request_count > 18
+        sleep(61 - Time.now.second)
+      end
 
-    response = yield
-    puts "Status: #{response.status_code}"
-    sleep 1
-    if response.status_code == 429
-      try += 1
-      set_request_count_to_maximum
-    else
-      break
+      response = yield
+      puts "Status: #{response.status_code}"
+      sleep 1
+      if response.status_code == 429
+        try += 1
+        set_request_count_to_maximum
+      else
+        break
+      end
     end
-  end
-  raise "Wtf?" unless response
-  response
+    raise "Wtf?" unless response
+    response
   end
 
   def get_request_count : Int32
-  incr = REDIS.incr(request_count_key).to_i
-  set_expire_request_count
-  incr
+    incr = REDIS.incr(request_count_key).to_i
+    set_expire_request_count
+    incr
   end
 
   def set_request_count_to_maximum
